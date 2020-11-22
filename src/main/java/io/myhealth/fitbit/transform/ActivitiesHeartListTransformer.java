777afa359613d8fc63a7c55bc53ea712a5a634c0
andras <fejes.andras@dev-labs.com>
2020-11-22 15:54:29 +0100

    Reversed order by time

--- src/main/java/io/myhealth/fitbit/transform/ActivitiesHeartListTransformer.java
+++ src/main/java/io/myhealth/fitbit/transform/ActivitiesHeartListTransformer.java
@@ -4,13 +4,23 @@ import com.fitbit.api.heart.ActivitiesHeart;
 import com.fitbit.api.heart.ActivitiesHeartList;
 import reactor.core.publisher.Mono;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ActivitiesHeartListTransformer implements Function<Mono<ActivitiesHeartList>, Mono<List<ActivitiesHeart>>> {
 
     @Override
     public Mono<List<ActivitiesHeart>> apply(Mono<ActivitiesHeartList> activitiesHeartList) {
-        return activitiesHeartList.map(ActivitiesHeartList::getActivitiesHeart);
+        return activitiesHeartList.map(this::transform);
+    }
+
+    private List<ActivitiesHeart> transform(ActivitiesHeartList activities) {
+        return activities
+                .getActivitiesHeart()
+                .stream()
+                .sorted(Comparator.comparing(ActivitiesHeart::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
